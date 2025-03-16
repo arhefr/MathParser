44d146e1c7a9a8df4b9fa21587520c07611169b8
arhefr <efremenkoarhip614090@gmail.com>
2025-03-16 19:02:03 +0300

    fix reverse function

--- tools.go
+++ tools.go
@@ -72,9 +72,7 @@ func train(expr []string) []string {
 func reverse(s string) string {
 	runes := []rune(s)
 	for i, j := 0, len(runes)-1; i < j; i, j = i+1, j-1 {
-		if string(runes[i]) != "(" && string(runes[i]) != ")" && string(runes[j]) != "(" && string(runes[j]) != ")" {
-			runes[i], runes[j] = runes[j], runes[i]
-		}
+		runes[i], runes[j] = runes[j], runes[i]
 	}
 	return string(runes)
 }
